6e1640b3ca8e123a9283ac14c25247088cc1e17c
IzzHrz <izzharizdev@gmail.com>
2020-10-24 22:56:29 +0800

    added Clarifai API for face detection model

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import './App.css';
+
+
+const app = new Clarifai.App({
+  apiKey: '4f65e4f902f64fff856ee0036f06926a'
+})
 
 const particlesOptions = {
   particles: {
@@ -18,21 +25,50 @@ const particlesOptions = {
   }
 }
 
-function App (){
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank/>
-      <ImageLinkForm />
-      
-       {/*
-      <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => {
+    // There was a successful response
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+    })
+    .catch(error => {
+    // There was an error
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank/>
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  } 
 }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import './Logo.css'
 
 function Logo() {
     return (
-        <div class="ma4 mt0">
+        <div className="ma4 mt0">
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa-3">
                     <img style={{paddingTop: '25px'}} alt='Logo' src={brain}/>
